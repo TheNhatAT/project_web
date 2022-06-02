1efb804ceb098b215fad76f20c51d8d05480153b
TheNhatAT <nhat0103st@gmail.com>
2022-06-02 16:01:47 +0700

    fix layout

--- client/src/pages/auth/components/Login.jsx
+++ client/src/pages/auth/components/Login.jsx
@@ -9,10 +9,10 @@ export default function Login() {
                     className="flex flex-col bg-white shadow-md px-4 sm:px-6 md:px-8 lg:px-10 py-8 rounded-3xl w-50 max-w-md"
                 >
                     <div className="font-medium self-center text-xl sm:text-3xl text-gray-800">
-                        Welcome Back
+                        Chào mừng bạn trở lại.
                     </div>
-                    <div className="mt-4 self-center text-xl sm:text-sm text-gray-800">
-                        Enter your credentials to access your account
+                    <div className="mt-4 self-center text-2xl sm:text-sm text-gray-800">
+                        Bạn cần đăng nhập để cho thuê phòng
                     </div>
 
                     <div className="mt-10">
@@ -21,7 +21,7 @@ export default function Login() {
                                 <label
                                     htmlFor="email"
                                     className="mb-1 text-xs tracking-wide text-gray-600"
-                                >E-Mail Address:</label
+                                >Email:</label
                                 >
                                 <div className="relative">
                                     <div
@@ -42,7 +42,7 @@ export default function Login() {
                                 <label
                                     htmlFor="password"
                                     className="mb-1 text-xs sm:text-sm tracking-wide text-gray-600"
-                                >Password:</label
+                                >Mật khẩu:</label
                                 >
                                 <div className="relative">
                                     <div className="inline-flex items-center justify-center absolute left-0 top-0 h-full w-10 text-gray-400">
@@ -65,7 +65,7 @@ export default function Login() {
                                 <button
                                     type="submit"
                                     className="flex mt-2 items-center justify-center focus:outline-none text-white text-sm sm:text-base bg-blue-500 hover:bg-blue-600 rounded-2xl py-2 w-full transition duration-150 ease-in">
-                                    <span className="mr-2 uppercase">Sign In</span>
+                                    <span className="mr-2 uppercase">Đăng nhập</span>
                                     <span>
                   <svg
                       className="h-6 w-6"
@@ -93,11 +93,11 @@ export default function Login() {
                         className="inline-flex items-center text-gray-700 font-medium text-xs text-center"
                     >
           <span className="ml-2"
-          >You don't have an account?
+          >Bạn chưa có tài khoản?
             <a
                 href="#"
                 className="text-xs ml-2 text-blue-500 font-semibold"
-            >Register now</a
+            >Đăng ký ngay</a
             ></span
           >
                     </a>
