effea256d791800d691c4880206a47223b7ae18e
TheNhatAT <nhat0103st@gmail.com>
2022-06-02 16:22:07 +0700

    fix title register page

--- client/src/pages/auth/components/Register.jsx
+++ client/src/pages/auth/components/Register.jsx
@@ -7,7 +7,7 @@ export default function Register () {
             <div className="h-screen bg-indigo-100 flex justify-center items-center">
                 <div className="lg:w-2/5 md:w-1/2 w-2/3">
                     <form className="bg-white p-10 rounded-lg shadow-lg min-w-full">
-                        <h1 className="text-center text-2xl mb-6 text-gray-600 font-bold font-sans">Formregister</h1>
+                        <h1 className="text-center text-2xl mb-6 text-gray-600 font-bold font-sans">Đăng ký tài khoản</h1>
                         <div>
                             <label className="text-gray-800 font-semibold block my-3 text-md" >Tên đăng nhập</label>
                             <input className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="username" id="username" placeholder="Tên đăng nhập" />
