45dd7dbed9102bfa3055c75c5949659ba3876c28
TheNhatAT <nhat0103st@gmail.com>
2022-06-16 23:33:39 +0700

    add api register

--- server/services/UserService.js
+++ server/services/UserService.js
@@ -6,24 +6,20 @@ exports.createOne = async (pathname, query, body) => {
     const {name, email, password, address, role, phone_number} = body;
 
     console.log('body: ', body)
-    let check_user = await conn.execute('SELECT * FROM `users` WHERE `email` = ?',
+    let [check_users, fields]  = await conn.execute('SELECT * FROM `users` WHERE `email` = ?',
         [email])
-    console.log('check_user: ', check_user);
-    if (check_user) {
+    console.log('check_user: ', check_users);
+    if (check_users.length !== 0) {
         throw new Error('Email have already registered!')
     }
 
     const hash_password = bcrypt.hashSync(password, 10);
 
-    let user = await conn.execute('INSERT INTO users SET ? ', {
-        name: name,
-        email: email,
-        password: hash_password,
-        address: address,
-        phone_number: phone_number,
-        role: role
-    });
-    console.log('user: ', user);
+    let [user] = await conn.execute('INSERT INTO `users` (name, email, password, address, role, phone_number) VALUES (?, ?, ?, ?, ?, ?) ',
+        [name, email, hash_password, address, role, phone_number]);
+
+    [user] = await conn.execute('SELECT * FROM `users` WHERE `id` = ?',
+        [user.insertId]);
     return user;
 }
 exports.getAllBoardingRoom = async (pathname, query, body) =>{
