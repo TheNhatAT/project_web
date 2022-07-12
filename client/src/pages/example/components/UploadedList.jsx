bc528b22db9753cbbb8e854253338e27fb7f87fa
TheNhatAT <nhat0103st@gmail.com>
2022-07-12 14:24:47 +0700

    add api for uploaded list page

--- client/src/pages/example/components/UploadedList.jsx
+++ client/src/pages/example/components/UploadedList.jsx
@@ -4,17 +4,15 @@ import { useEffect, useState } from "react";
 
 export default function UploadedList() {
 
-    const [data1, setData1] = useState([]);
-    // const [image, setImage] = useState([]);
+    const [boardingRooms, setBoardingRooms] = useState([]);
 
     useEffect(() => {
         async function fetchData() {
-            return await axios.get(`http://localhost:8000/boarding-rooms/id/1`);
+            let ownerId = localStorage.getItem('userId')
+            return await axios.get(`http://localhost:8000/users/boarding-rooms?owner_id=${ownerId}`);
         }
         fetchData().then((res) => {
-            setData1(res.data.content.boarding_room);
-
-
+            setBoardingRooms(res.data.content);
         });
     }, []);
     return (
@@ -25,21 +23,15 @@ export default function UploadedList() {
                 </div>
 
 
-                <div class="w-100 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-
-                    <a href="#" class="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
-                        Phòng 1
-                    </a>
-                    <a href="#" class="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
-                        Phòng 2
-                    </a>
-                    <a href="#" class="block w-full px-4 py-2 rounded-b-lg cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
-                        Phòng 3
-                    </a>
+                <div className="w-100 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                    {boardingRooms.map((item, index) => {
+                        return (
+                            <a key={index} href="#" className="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
+                                {item.name}
+                            </a>
+                        )
+                    })}
                 </div>
-
-
-
             </div>
         </>
     )
