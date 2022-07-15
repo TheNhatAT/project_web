e3655b77ae9a7b27d507f87cec74ebfeaa530fc7
TheNhatAT <nhat0103st@gmail.com>
2022-07-16 00:14:11 +0700

    fix update boarding-room

--- client/src/pages/example/components/UploadedList.jsx
+++ client/src/pages/example/components/UploadedList.jsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import {NavLink} from "react-router-dom";
 
 export default function UploadedList() {
 
@@ -26,9 +27,9 @@ export default function UploadedList() {
                 <div className="w-100 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
                     {boardingRooms.map((item, index) => {
                         return (
-                            <a key={index} href="#" className="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
+                            <NavLink key={index} to={`/boarding-room/update/${item.id}`} className="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
                                 {item.name}
-                            </a>
+                            </NavLink>
                         )
                     })}
                 </div>
